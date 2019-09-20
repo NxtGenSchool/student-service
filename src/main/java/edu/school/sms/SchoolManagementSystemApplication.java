80141eae3c731435adafd92ab402f438db0c58ef
Shubham Singh <shubham.suresh.singh@gmail.com>
2019-09-20 15:17:13 -0400

    Disable spring security temporarily

--- src/main/java/edu/school/sms/SchoolManagementSystemApplication.java
+++ src/main/java/edu/school/sms/SchoolManagementSystemApplication.java
@@ -2,8 +2,9 @@ package edu.school.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SchoolManagementSystemApplication {
 
     public static void main(String[] args) {
