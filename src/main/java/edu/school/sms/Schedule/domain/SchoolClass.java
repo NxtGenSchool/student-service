0311749413d5e27f35a3c8c9428460a021dd0775
Shubham Singh <shubham.suresh.singh@gmail.com>
2019-09-21 17:32:07 -0400

    Add SchoolClass and SchoolClassTime entities

---
+++ src/main/java/edu/school/sms/Schedule/domain/SchoolClass.java
@@ -0,0 +1,90 @@
+package edu.school.sms.Schedule.domain;
+
+import edu.school.sms.course.domain.Course;
+import edu.school.sms.student.domain.Student;
+
+import javax.persistence.*;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class SchoolClass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long uidPk;
+    @OneToOne
+    private Course course;
+    private long classId;
+    private String roomNumber;
+    @OneToMany(mappedBy = "schoolClass")
+    private List<SchoolClassTime> schoolClassTimes;
+    private Duration duration;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public long getUidPk() {
+        return uidPk;
+    }
+
+    public void setUidPk(long uidPk) {
+        this.uidPk = uidPk;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public long getClassId() {
+        return classId;
+    }
+
+    public void setClassId(long classId) {
+        this.classId = classId;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public List<SchoolClassTime> getSchoolClassTimes() {
+        return schoolClassTimes;
+    }
+
+    public void setSchoolClassTimes(List<SchoolClassTime> schoolClassTimes) {
+        this.schoolClassTimes = schoolClassTimes;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
---
+++ src/main/java/edu/school/sms/Schedule/domain/SchoolClassTime.java
@@ -0,0 +1,49 @@
+package edu.school.sms.Schedule.domain;
+
+import javax.persistence.*;
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+@Entity
+public class SchoolClassTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long uidPk;
+    private DayOfWeek dayOfWeek;
+    private LocalTime localTime;
+    @ManyToOne
+    private SchoolClass schoolClass;
+
+    public long getUidPk() {
+        return uidPk;
+    }
+
+    public void setUidPk(long uidPk) {
+        this.uidPk = uidPk;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(DayOfWeek dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public LocalTime getLocalTime() {
+        return localTime;
+    }
+
+    public void setLocalTime(LocalTime localTime) {
+        this.localTime = localTime;
+    }
+
+    public SchoolClass getSchoolClass() {
+        return schoolClass;
+    }
+
+    public void setSchoolClass(SchoolClass schoolClass) {
+        this.schoolClass = schoolClass;
+    }
+}
