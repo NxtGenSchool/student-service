f4dd87c19ff77ca6f08156b4f887e3737cac9408
Shubham Singh <shubham.suresh.singh@gmail.com>
2019-09-22 17:05:01 -0400

    Some refactoring in schedule package

--- src/main/java/edu/school/sms/schedule/repository/SchoolClassRepository.java
+++ src/main/java/edu/school/sms/schedule/repository/SchoolClassRepository.java
@@ -1,11 +1,9 @@
 package edu.school.sms.schedule.repository;
 
-import edu.school.sms.schedule.domain.ClassEnrollment;
+import edu.school.sms.schedule.domain.SchoolClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClassEnrollmentRepository extends JpaRepository<ClassEnrollment, Long> {
-
-    
+public interface SchoolClassRepository extends JpaRepository<SchoolClass, Long> {
 }
--- src/main/java/edu/school/sms/schedule/repository/SchoolClassRespository.java
+++
@@ -1,9 +0,0 @@
-package edu.school.sms.schedule.repository;
-
-import edu.school.sms.schedule.domain.SchoolClass;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SchoolClassRespository extends JpaRepository<SchoolClass, Long> {
-}
