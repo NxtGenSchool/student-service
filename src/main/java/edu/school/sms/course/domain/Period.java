41979b8d8e147edc9b108a526b09cbd67eeaf86c
Shubham Singh <shubham.suresh.singh@gmail.com>
2019-10-01 20:17:45 -0400

    Add /student/{studentId}/schedule API

---
+++ src/main/java/edu/school/sms/course/domain/Period.java
@@ -0,0 +1,62 @@
+package edu.school.sms.course.domain;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Period {
+
+    private String subjectCode;
+    private String subjectName;
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private String location;
+
+    public String getSubjectCode() {
+        return subjectCode;
+    }
+
+    public void setSubjectCode(String subjectCode) {
+        this.subjectCode = subjectCode;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
