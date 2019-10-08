343572f8a803170340ff5d9fd9793dabeedd7819
Shubham Singh <shubham.suresh.singh@gmail.com>
2019-10-08 12:31:49 -0400

    add teacher to a course
    teacher package will have all the functionality related to teacher module in
    the system. We would begin by linking teacher to courses, but soon teacher
    will have its own api and will also have features like ability to make
    announcements.

---
+++ src/main/java/edu/school/sms/teacher/domain/Teacher.java
@@ -0,0 +1,101 @@
+package edu.school.sms.teacher.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import edu.school.sms.course.domain.Course;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "UID_PK")
+    @JsonIgnore
+    private Long uidPk;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String teacherId;
+    private String emailAddress;
+    private String phoneNumber;
+    private String officeLocation;
+    @OneToMany(mappedBy = "teacher")
+    @JsonBackReference
+    private List<Course> courses;
+
+    public Long getUidPk() {
+        return uidPk;
+    }
+
+    public void setUidPk(Long uidPk) {
+        this.uidPk = uidPk;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(String teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOfficeLocation() {
+        return officeLocation;
+    }
+
+    public void setOfficeLocation(String officeLocation) {
+        this.officeLocation = officeLocation;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+}
